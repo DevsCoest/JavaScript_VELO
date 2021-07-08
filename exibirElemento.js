4d535f52f71de1abdea987f7fb8b2550ddd59cdd
Devs Coest Studio <87096563+DevsCoest@users.noreply.github.com>
2021-07-08 16:46:48 -0300

    Exibir elemento
    Exibir elemento(s) após clique.

---
+++ exibirElemento.js
@@ -0,0 +1,15 @@
+//Código para exibir (elementos) após clicar no (elemento)
+// #DevsCoestStudio :P
+
+
+$w.onReady(function () {
+	
+	$w('#NomeDoElemento').onClick(() =>{
+
+		$w('#NomeDoElemento').expand();
+	}
+		
+
+	)
+
+});
